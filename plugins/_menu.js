15898fc243815831c8fc4a0fbc864fe36f868581
FamzyHack <155462423+FamzyHack@users.noreply.github.com>
2024-09-14 12:26:49 +0100

    Update _menu.js

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -24,7 +24,7 @@ bot.addCommand(
     })
     const [date, time] = getDate()
     let CMD_HELP = `╭────────────────╮
-						ʟᴇᴠᴀɴᴛᴇʀ
+					TREASURE404DANGER
 ╰────────────────╯
 
 ╭────────────────
@@ -93,7 +93,7 @@ bot.addCommand(
       }
     })
     const [date, time] = getDate()
-    let msg = `\`\`\`╭═══ LEVANTER ═══⊷
+    let msg = `\`\`\`╭═══ TREASURE404DANGER ═══⊷
 ┃❃╭──────────────
 ┃❃│ Prefix : ${PREFIX}
 ┃❃│ User : ${message.pushName}
